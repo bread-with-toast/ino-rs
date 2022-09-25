9d96389c499b23b90ca9f948eb9d9a8e2a77ac73
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-09-25 16:59:31 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 mod ino_rs;
 
 fn main() {
-    let trig = 10;
-    let echo = 11;
-    let red = 3;
+	let red = 3;
+	let green = 5;
+	let blue = 6;
 
-    ino_rs::begin_setup();
-    ino_rs::def_pin(trig, false);
-    ino_rs::def_pin(echo, true);
-    ino_rs::def_pin(red, false);
-    ino_rs::end_function();
+	ino_rs::begin_setup();
+	ino_rs::def_pin(red, false);
+	ino_rs::def_pin(green, false);
+	ino_rs::def_pin(blue, false);
+	ino_rs::end_function();
 
-    ino_rs::begin_loop();
-    ino_rs::off(trig);
-    ino_rs::halt(0.01);
-    ino_rs::on(trig);
-    ino_rs::halt(0.01);
-    ino_rs::off(trig);
-
-    ino_rs::pulse_in_var("time_taken".to_string(), "int".to_string(), echo.into());
-
-    ino_rs::if_cond("time_taken < 500".to_string(), "digitalWrite(3, HIGH)".to_string());
-    ino_rs::if_cond("time_taken > 500".to_string(), "digitalWrite(3, LOW)".to_string());
-
-    ino_rs::end_function();
-
+	ino_rs::begin_loop();
+	ino_rs::on(red);
+	ino_rs::off(green);
+	ino_rs::off(blue);
+	ino_rs::halt(1.0);
+	
+	ino_rs::on(green);
+	ino_rs::off(red);
+	ino_rs::off(blue);
+	ino_rs::halt(1.0);
+	
+	ino_rs::on(blue);
+	ino_rs::off(green);
+	ino_rs::off(red);
+	ino_rs::halt(1.0);
+	ino_rs::end_function();
 }
